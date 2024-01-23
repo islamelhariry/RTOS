2e0a86d7c841bfd25dc6225becc54d15557cc063
islamelhariry <islam.elhariry@yahoo>
2024-01-23 21:45:10 +0000

    first commit

---
+++ Objects/lab1.d
@@ -0,0 +1,6 @@
+.\objects\lab1.o: Lab1.c
+.\objects\lab1.o: E:\Campus\Courses\Embedded\TM4C123GXL\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lab1.o: ../inc/BSP.h
+.\objects\lab1.o: ../inc/Profile.h
+.\objects\lab1.o: Texas.h
+.\objects\lab1.o: ../inc/CortexM.h
